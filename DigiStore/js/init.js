0e06f0fe94d03e8fc63efb301a2aece55e5215eb
delmond isaacs <delmondisaacs@live.ca>
2016-09-18 12:27:12 -0400

    help

---
+++ DigiStore/js/init.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.carousel.carousel-slider').slider({ full_width: true });
+        $('.carousel').carousel();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
