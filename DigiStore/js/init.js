05a41dfeec4a8bfc292533f8b86ee5beba74c529
KevinN <KevinNewbury>
2016-09-23 18:28:35 -0400

    updates to storepage

--- DigiStore/js/init.js
+++ DigiStore/js/init.js
@@ -5,6 +5,10 @@
         $('.carousel').carousel();
         $('.parallax').parallax();
         $('#imgat').materialbox();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
         //$('.range-field').range({color:#1565c0});
     }); // end of document ready
 })(jQuery); // end of jQuery name space
