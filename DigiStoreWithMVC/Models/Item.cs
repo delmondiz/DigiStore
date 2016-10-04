c9ce127bceffff86234f286a7ac9435621127225
Evan Christopher <ewchris101@gmail.com>
2016-10-03 21:04:55 -0400

    Fixed issues with data types. Dun goofed when I created the DB model and made everything strings. its all good now

--- DigiStoreWithMVC/Models/Item.cs
+++ DigiStoreWithMVC/Models/Item.cs
@@ -17,8 +17,8 @@ namespace DigiStoreWithMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
-        public string Weight { get; set; }
+        public decimal Price { get; set; }
+        public Nullable<double> Weight { get; set; }
         public int OrderId { get; set; }
         public int UserId { get; set; }
     
--- DigiStoreWithMVC/Models/User.cs
+++ DigiStoreWithMVC/Models/User.cs
@@ -35,7 +35,7 @@ namespace DigiStoreWithMVC.Models
         public string City { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
-        public string PhoneNumber { get; set; }
+        public Nullable<int> PhoneNumber { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PaymentMethod> PaymentMethods { get; set; }
