3724c0b881f9ca259beb10eb8ac8578d66cd07cc
Kevin <crasykid2@hotmail.com>
2016-10-05 13:01:43 -0400

    - Copied over Javascipt, CSS, image files - Downloaded all libraries (jquery, materialize, icons) to local folders (excluding google maps)

---
+++ DigiStoreWithMVC/Scripts/init.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+    $(function () {
+        $('.button-collapse').sideNav();
+        $('.carousel.carousel-slider').slider();
+        $('.carousel').carousel();
+        $('.parallax').parallax();
+        $('#imgat').materialbox();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+        $('.dropify').dropify();
+        //$('.range-field').range({color:#1565c0});
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
