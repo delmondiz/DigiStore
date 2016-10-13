e3fc832b3c2fa98b3b256c3c781e6d1bd2e01f53
Kevin <crasykid2@hotmail.com>
2016-10-13 13:04:06 -0400

    - I CAN'T GET THE UPDATED ROUTECONFIG NOR LAYOUT. T_T - Login and Register work; Layout Log In/Log out should change depending on whether the user is logged in/out

--- DigiStoreWithMVC/Models/AccountViewModels.cs
+++ DigiStoreWithMVC/Models/AccountViewModels.cs
@@ -53,11 +53,6 @@ namespace DigiStoreWithMVC.Models
         [EmailAddress]
         public string Email { get; set; }
 
-        //[Required]
-        //[DataType(DataType.Text)]
-        //[Display(Name = "Username")]
-        //public string Username { get; set; }
-
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
