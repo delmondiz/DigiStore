3b8f6a908750266d12316f68cb9a70f5ee6eae64
Kevin <crasykid2@hotmail.com>
2016-10-18 04:56:54 -0400

    - Store Inventory is Finished - There's a new feature! :)

--- DigiStoreWithMVC/Controllers/StoreController.cs
+++ DigiStoreWithMVC/Controllers/StoreController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using System.Threading.Tasks;
 using System.Net;
+using System.Data.Entity;
 
 namespace DigiStoreWithMVC.Controllers
 {
@@ -21,13 +22,37 @@ namespace DigiStoreWithMVC.Controllers
             using (DigiStoreDBModelContainer db = new DigiStoreDBModelContainer())
             {
                 User checkUser = (from u in db.Users where u.UserName == storeName select u).FirstOrDefault();
-                
+
                 if (checkUser != null)
                 {
                     return View(checkUser);
                 }
             }
             return View();
+        }
+
+        public ActionResult RandomStore()
+        {
+            int max = db.Users.Count();
+            Random rand = new Random();
+            int randUserNum = rand.Next(0, (max - 1));
+            int count = 0;
+            User randomUser = null;
+            do
+            {
+                randomUser = (from u in db.Users
+                              where (u.Id == randUserNum)
+                              select u).FirstOrDefault();
+                if (count > 1000)
+                    randomUser = new User();
+                count++;
+            } while (randomUser == null);
+
+            if (count > 1000)
+                return RedirectToActionPermanent("Index", "Home", new { controller = "Home", action = "Index" });
+
+            return RedirectToAction("Index", "Store", new { storeName = randomUser.UserName });
+            //return View(randomUser.UserName);   
         }
 
         public ActionResult StoreInventory()
@@ -51,16 +76,68 @@ namespace DigiStoreWithMVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult StoreInventory([Bind(Include = "Id")] Item item)
+        public ActionResult StoreInventory(Item item)
         {
-            if (ModelState.IsValid)
+            if (User.Identity.IsAuthenticated)
             {
-                db.Items.Add(item);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    // Get our current user.
+                    User user = (from u in db.Users where u.Email == User.Identity.Name select u).FirstOrDefault();
+                    // Add the item to our current user.
+                    user.Items.Add(item);
+                    // Save the changes to the DB.
+                    db.SaveChanges();
+                    // Return the user to the Store Inventory
+                    return View(user);
+                }
+                return View(item);
             }
+            else
+                return RedirectToAction("Index", "Home");
+        }
 
-            return View(item);
+
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateItem([Bind(Include = "Id,Name,Description,Price,Weight,Quantity,ImagePath")]Item item)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                User user = (from u in db.Users where u.Email == User.Identity.Name select u).FirstOrDefault();
+                if (ModelState.IsValid)
+                {
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return View("StoreInventory", user);
+                }
+                //ViewBag.OrderId = new SelectList(db.Orders, "Id", "Id", item.OrderId);
+                //ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", item.UserId);
+                return View("StoreInventory", user);
+            }
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteItem([Bind(Include = "Id")]Item item)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                User user = (from u in db.Users where u.Email == User.Identity.Name select u).FirstOrDefault();
+                Item dbItem = (from i in db.Items where i.Id == item.Id select i).FirstOrDefault();
+                user.Items.Remove(dbItem);
+                db.SaveChanges();
+                return View("StoreInventory", user);
+            }
+            else
+                return RedirectToAction("Index", "Home");
         }
 
         public ActionResult ShoppingCart()
