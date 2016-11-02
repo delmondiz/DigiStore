5549f6e19436dde6120e6ff95b91d51f8f2d86fa
delmond isaacs <delmondisaacs@live.ca>
2016-11-02 09:32:57 -0400

    update with new paypal code

---
+++ DigiStoreWithMVC/Controllers/PaypalController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiStoreWithMVC.Controllers
+{
+    public class PaypalController : Controller
+    {
+        // GET: Paypal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
