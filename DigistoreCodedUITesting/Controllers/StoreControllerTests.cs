e324323b4edd2a66343a898d9869efa778524ef1
Evan Christopher <ewchris101@gmail.com>
2016-11-13 14:00:51 -0500

    Begin adding unit tests to store controller actions

---
+++ DigistoreCodedUITesting/Controllers/StoreControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigiStoreWithMVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiStoreWithMVC.Controllers.Tests
+{
+    [TestClass()]
+    public class StoreControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RandomStoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StoreInventoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StoreInventoryTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateItemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteItemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShoppingCartTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditStoreInfoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditStoreHoursTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddToCartTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
