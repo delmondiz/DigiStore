944da932eb345524900e68a5a66478fd8376f272
Kevin <crasykid2@hotmail.com>
2016-12-13 23:41:49 -0500

    Code Cleanup

--- DigiStoreWithMVC/Models/ItemMetaData.cs
+++ DigiStoreWithMVC/Models/ItemMetaData.cs
@@ -29,20 +29,6 @@ namespace DigiStoreWithMVC.Models
         [Required]
         [Display(Name = "Image Filepath")]
         public string ImagePath { get; set; }
-
-        //public Item Ite{
-        //   get {return ite;}
-        //   set {ite = value;}
-        //}
-
-        //private Item ite = new Item();
-
-        //public ItemMetaData() {        }
-        //public ItemMetaData(Item items, int quantity)
-        //{
-        //    this.ite = items;
-        //    this.Quantity = quantity;
-        //}
     }
 
          
